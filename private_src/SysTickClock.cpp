19d42869030693d32aeaffec5a81ecf24972bcfb
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-11 13:38:28 +0800

    DI_TaskManager().SuspendAllTask();     __disable_irq();     base::Guard g{         [&]()         {             DI_TaskManager().ResumeAllTask();             __enable_irq();         }};

--- private_src/SysTickClock.cpp
+++ private_src/SysTickClock.cpp
@@ -1,6 +1,7 @@
 #include "SysTickClock.h"
 #include <bsp-interface/di/delayer.h>
 #include <bsp-interface/di/interrupt.h>
+#include <bsp-interface/di/task.h>
 
 extern "C"
 {
@@ -34,22 +35,12 @@ void bsp::SysTickClock::AddSystemTime()
 bsp::SysTickClock &bsp::SysTickClock::Instance()
 {
     class Getter :
-        public base::SingletonGetter<SysTickClock>
+        public bsp::TaskSingletonGetter<SysTickClock>
     {
     public:
         std::unique_ptr<SysTickClock> Create() override
         {
             return std::unique_ptr<SysTickClock>{new SysTickClock{}};
-        }
-
-        void Lock() override
-        {
-            DI_DisableGlobalInterrupt();
-        }
-
-        void Unlock() override
-        {
-            DI_EnableGlobalInterrupt();
         }
     };
 
@@ -78,7 +69,15 @@ uint32_t bsp::SysTickClock::CurrentValue() const
 
 void bsp::SysTickClock::SetElapsedHandler(std::function<void()> func)
 {
-    bsp::GlobalInterruptGuard g;
+    DI_TaskManager().SuspendAllTask();
+    __disable_irq();
+    base::Guard g{
+        [&]()
+        {
+            DI_TaskManager().ResumeAllTask();
+            __enable_irq();
+        }};
+
     _elapsed_handler = func;
 }
 
